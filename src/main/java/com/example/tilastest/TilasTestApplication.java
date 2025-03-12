18f93aeaa9dcc0379f2c1b40bd263c9a986571a3
Xancel <108331256+XancelZC@users.noreply.github.com>
2025-03-12 19:18:28 +0800

    新建一个样例项目

---
+++ src/main/java/com/example/tilastest/TilasTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tilastest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilasTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TilasTestApplication.class, args);
+    }
+
+}
