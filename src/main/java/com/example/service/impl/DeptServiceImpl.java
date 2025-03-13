3dc07f527de314e790354caa86aca9487d952326
Xancel <108331256+XancelZC@users.noreply.github.com>
2025-03-13 19:15:12 +0800

    完成查询部门信息功能

---
+++ src/main/java/com/example/service/impl/DeptServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.service.impl;
+
+import com.example.mapper.DeptMapper;
+import com.example.pojo.Dept;
+import com.example.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class DeptServiceImpl implements DeptService {
+    @Autowired
+    private DeptMapper deptMapper;
+
+    @Override
+    public List<Dept> list() {
+        return deptMapper.list();
+    }
+}
